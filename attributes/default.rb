ec80e4591133eb8a1ab8b4228a1729489f1ed8a2
anant jain <anant.jain@optimusinfo.com>
2013-05-17 12:52:10 +0530

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default["guacamole"]["war_file"] = "/var/lib/guacamole/guacamole.war"
+default["guacamole"]["tar_file"] = "guacamole-0.7.0-ubuntu-12.04-amd64"
+default["guacamole"]["url"] = "http://sourceforge.net/projects/guacamole/files/current/binary/ubuntu-12.04-amd64/guacamole-0.7.0-ubuntu-12.04-amd64.tar.gz"
